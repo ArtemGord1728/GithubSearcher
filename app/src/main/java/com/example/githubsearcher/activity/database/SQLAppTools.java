02f3b71d623e51b98bf40a529aa6642d931d679a
ArtemGord1728 <artemseraf01@mail.ru>
2019-10-14 00:30:02 +0300

    changes

--- app/src/main/java/com/example/githubsearcher/activity/database/SQLAppTools.java
+++ app/src/main/java/com/example/githubsearcher/activity/database/SQLAppTools.java
@@ -1,4 +1,46 @@
 package com.example.githubsearcher.activity.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.githubsearcher.activity.constants.ApiConstants;
+import com.example.githubsearcher.activity.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SQLAppTools {
+
+    private SQLiteDatabase sql_db;
+
+    public SQLAppTools(final Context context) {
+        sql_db = new SQLExecution(context).getWritableDatabase();
+    }
+
+    public void addToFavorites(User user) {
+        ContentValues storeForDB = new ContentValues();
+        storeForDB.put("username", user.getLogin());
+        sql_db.insert(String.valueOf(ApiConstants.TABLE_NAME), null, storeForDB);
+    }
+
+    public void removeUserFromID(String fieldName, long id){
+        sql_db.delete(String.valueOf(ApiConstants.TABLE_NAME), fieldName + " = " + id, null);
+    }
+
+    public List<User> getAllFavorite() {
+        final Cursor cursor = sql_db.query(String.valueOf(ApiConstants.TABLE_NAME), null, null, null, null, null, "username DESC");
+        List<User> users = new ArrayList<>();
+
+        if(cursor.moveToFirst()){
+            while(cursor.moveToNext())
+            {
+                users.add(new User(
+                        cursor.getString(cursor.getColumnIndex("username"))));
+            }
+        }
+        cursor.close();
+        return users;
+    }
 }
--- app/src/main/java/com/example/githubsearcher/activity/database/SQLExecution.java
+++ app/src/main/java/com/example/githubsearcher/activity/database/SQLExecution.java
@@ -1,4 +1,26 @@
 package com.example.githubsearcher.activity.database;
 
-public class SQLExecution {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.githubsearcher.activity.constants.ApiConstants;
+
+public class SQLExecution extends SQLiteOpenHelper {
+
+    public SQLExecution(final Context context){
+        super(context, "githubDB", null,1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + ApiConstants.TABLE_NAME + "(" +
+                "username VARCHAR(20)" +
+                ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
